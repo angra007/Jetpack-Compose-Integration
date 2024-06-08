79ee519cabbbb4d217bbfbc1550359878a73d79c
Ankit Angra <angraankit@gmail.com>
2024-06-08 03:27:48 -0400

    Added Jetpack Compose in an existing project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
 }
 
 android {
@@ -33,9 +34,25 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
+    }
 }
 
 dependencies {
+
+    implementation ("androidx.compose.ui:ui:1.6.7")
+    implementation ("androidx.compose.runtime:runtime:1.6.7")
+    implementation ("androidx.compose.foundation:foundation:1.6.7")
+    implementation ("androidx.compose.animation:animation:1.6.7")
+    implementation ("androidx.compose.material:material:1.6.7")
+    implementation ("androidx.compose.material3:material3:1.2.1")
+    implementation("androidx.activity:activity-compose:1.9.0")
 
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
--- app/src/main/java/com/codefylabs/www/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/codefylabs/www/jetpackcompose/MainActivity.kt
@@ -2,10 +2,21 @@ package com.codefylabs.www.jetpackcompose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            HelloWorld()
+        }
     }
+}
+
+
+@Composable
+fun HelloWorld() {
+    Text("Hello World!!")
 }
